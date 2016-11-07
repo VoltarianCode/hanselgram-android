c4fa164b008fe6e7b9d0d11e0fe33ac962182e9e
VoltarianCode <illya.semenkov@gmail.com>
2016-11-07 04:45:03 -0500

    added theme

--- app/src/main/java/app/voltarian/hanselgram/MainActivity.java
+++ app/src/main/java/app/voltarian/hanselgram/MainActivity.java
@@ -1,5 +1,6 @@
 package app.voltarian.hanselgram;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
+
+
     }
 }
